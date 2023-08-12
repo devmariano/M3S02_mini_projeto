5f999159a36c111d723429d0b829e99936e5ee18
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-12 17:05:16 -0300

    adicioando useState na search

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import './searchBar.css';
 
 function SearchBar() {
+  const[searchValue, setSearchValue] = useState('');
+
   return (
     <form className="search-bar">
       <input 
         type="search" 
+        value={searchValue}
         className="searh__input" 
         placeholder="Buscar Produtos"
+        onChange={ ({target}) => setSearchValue(target.value) }
         required
       />
-
       <button type="submit" className="search__button">
         <BsSearch/>
       </button>
