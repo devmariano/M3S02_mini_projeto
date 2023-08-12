8335a9cc0aa180ce8cdbf8f57be60cae0fadbf17
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-12 18:41:37 -0300

    estilizado product card

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import './ProductCard.css';
+import {BsCartPlusFill} from 'react-icons/bs';
 
 function ProductCard() {
   return ( 
-    <section className="product__card">
+    <section className="product-card">
       <img src="https://http2.mlstatic.com/D_850103-MLB50444835173_062022-W.jpg" alt="product" className="card__image" />
       <div className="card__infos">
         <h2 className="card__price">R$200.00</h2>
-        <h2 className="card__tittle">Titulo do card</h2>
+        <h2 className="card__title">Titulo do card</h2>
       </div>
-      <button type="button" className="button_add-cart">+</button>
+      <button type="button" className="button__add-cart">
+        <BsCartPlusFill/>
+      </button>
     </section>
   );
 }
