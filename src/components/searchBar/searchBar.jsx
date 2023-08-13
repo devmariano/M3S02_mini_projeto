ee75dbe1c4fd96552b3c4753245eeaabcf9d27ab
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-13 08:12:55 -0300

    criada logica de pesquisa do searchbar

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import './searchBar.css';
+import fetchProducts from '../../api/fetchProdutcs';
 
 function SearchBar() {
   const[searchValue, setSearchValue] = useState('');
 
+  const handleSearch = async(event) => {
+    event.preventDefault();
+
+    const products = await fetchProducts(searchValue);
+    console.log(products);
+    setSearchValue('');
+  };
+
   return (
-    <form className="search-bar">
+    <form className="search-bar" onSubmit={handleSearch}>
       <input 
         type="search" 
         value={searchValue}
