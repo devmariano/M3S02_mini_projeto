15f94dcea2509fa12ce3742f41c6425320a94063
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-13 13:01:24 -0300

    adicionado acumulador por reduce total do cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,10 +3,15 @@ import React, { useContext } from 'react';
 import './Cart.css';
 import CartItem from '../CartItem/CartItem';
 import AppContext from '../context/AppContext';
+import formatCurrency from '../../utils/formatCurrency';
 
 function Cart() {
 
   const {cartItems} = useContext(AppContext);
+  // acc é o acumulador do reduce, neste caso começa com 0
+  const totalPrice = cartItems.reduce((acc, item) => {
+    return item.price + acc;
+  },0);
 
   return ( 
     <section className="cart">
@@ -14,7 +19,7 @@ function Cart() {
         {cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />)}
       </div>
 
-      <div className="cart-resume">Resumo do carrinho</div>
+      <div className="cart-resume">{formatCurrency(totalPrice, 'BRL')}</div>
     </section>
   );
 }
