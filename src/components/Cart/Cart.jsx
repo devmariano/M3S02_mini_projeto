8e5736a268992d3f59655bd124ef3982f14b4e09
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-13 11:02:14 -0300

    estilização da section do carrinho

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './Cart.css';
+
 function Cart() {
   return ( 
-    <section>
-      carrinho
+    <section className="cart">
+      <div className="cart-items">Itens do carrinho</div>
+
+      <div className="cart-resume">Resumo do carrinho</div>
     </section>
   );
 }
