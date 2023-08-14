1bb6f01b064c833f8bcf8e4c4a6b55e5cb0e9d07
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-13 21:10:45 -0300

    adicionado contagem de itens no cartbutton

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {BsCart2} from 'react-icons/bs';
 import './CartButton.css';
+import AppContext from '../context/AppContext';
 
 function CartButton() {
+
+  const {cartItems} = useContext(AppContext);
+
   return (  
     <button type="button" className="cart__button">
       <BsCart2/>
-      <span className="cart-status">10</span>
+      <span className="cart-status">{cartItems.length}</span>
     </button>
   );
 }
