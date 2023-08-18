dd8777c50c80bd1dd59d6a1c478d04624f30c151
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-17 21:39:47 -0300

    criado o page not found

--- src/pages/pageNotFound.jsx
+++ src/pages/pageNotFound.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import error from '../images/error404.jpg';
+import './pageNotFound.css';
 
 
 
 function PageNotFound() {
   return (
-    <>
-      Página 404 nada encontrado!
-    </>
+    <div>
+      <Header />
+      <div className="not-found-container">
+        <p className="not-found-text">Ops! Página não encontrada.</p>
+        <img
+          src={error}
+          alt="404 Not Found"
+          className="not-found-image"
+        />
+        <p className="not-found-suggestion">Que tal explorar outras páginas?</p>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
