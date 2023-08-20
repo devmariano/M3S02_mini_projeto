628c557557627f05d000b6b603dfca395619f09f
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-20 18:50:46 -0300

    desativado header do jest

--- src/tests/header.testoff.js
+++ src/tests/header.testoff.js
@@ -1,46 +1,43 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import Header from '../components/Header/Header';
-import * as AppContext from '../context/AppContext';
+import Header from '../components/Header/Header'; // Verifique o caminho correto
+import Provider from '../context/Provider'; 
 
-// Mock o módulo do contexto inteiro
-jest.mock('../context/AppContext');
-
-test('renderiza o header corretamente', () => {
-  // Configure o retorno mockado do useContext
-  AppContext.useContext.mockReturnValue({
+test('renderiza o componente Header corretamente', () => {
+  // Simule os valores do contexto que você espera para o teste
+  const mockContextValues = {
     cartItems: [
       {
-        'id': 'MLB3388244719',
-        'title': 'Notebook Acer Nitro 5 An515-58w3 I5 8gb 512gb Rtx3050 15.6',
-        'thumbnail': 'http://http2.mlstatic.com/D_951215-MLU70292400551_072023-I.jpg',
-        'currency_id': 'BRL',
-        'price': 4840,
-        'quantity': 2,
-      }
-    ], // Valores simulados para o teste
-    isCartVisible: false,
-    setIsCartVisible: jest.fn(),
-  });
+        id: 'MLB3388244719',
+        title: 'Notebook Acer Nitro 5 An515-58w3 I5 8gb 512gb Rtx3050 15.6',
+        thumbnail: 'http://http2.mlstatic.com/D_951215-MLU70292400551_072023-I.jpg',
+        currency_id: 'BRL',
+        price: 4840,
+        quantity: 1,
+      },
+    ],
+    isCartVisible: true,
+    totalPrice: 4840, // Simule o valor total do carrinho
+  };
 
+  // Renderize o componente envolto pelo provedor de contexto simulado e MemoryRouter
   const { getByAltText, queryByTestId } = render(
     <MemoryRouter>
-      <Header />
+      <Provider value={mockContextValues}>
+        <Header />
+      </Provider>
     </MemoryRouter>
   );
 
-  // Seu código de teste aqui
-
-  // Exemplo de verificação: Verifique se a imagem do logo está presente
+  // Verifique se o logotipo está presente
   const logoImage = getByAltText('Logotipo');
   expect(logoImage).toBeInTheDocument();
 
-  // Exemplo de verificação: Verifique se o componente SearchBar não está presente na rota de carrinho
-  const searchBar = queryByTestId('search-bar'); // Usar queryByTestId para verificar se não está presente
+  // Verifique se o componente SearchBar não está presente na rota de carrinho
+  const searchBar = queryByTestId('search-bar');
   expect(searchBar).toBeNull();
 
-  // Exemplo de verificação: Verifique se o componente CartButton está presente
-  const cartButton = getByTestId('cart-button');
+  // Verifique se o componente CartButton está presente
+  const cartButton = queryByTestId('cart-button');
   expect(cartButton).toBeInTheDocument();
-});
